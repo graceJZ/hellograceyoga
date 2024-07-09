5210f1a6657711c553ea95eb654902256f0c5a67
graceJZ <jie.p.zhao@gmail.com>
2024-07-09 02:36:19 -0400

    intro adjustment

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,11 +3,10 @@ import './Intro.css'; // Assume you have a CSS file for specific styles
 
 const Intro = () => {
   return (
-    <div id="intro" className="intro-section" style={{ backgroundImage: `url('https://raw.githubusercontent.com/graceJZ/hellograceimages/main/background.jpg')` }}>
+    <div id="intro" className="intro-section">
       <div className="overlay">
         <h1>Hello Grace Yoga</h1>
         <p>Classical Yoga For Modern Life</p>
-        {/* <button className="cta-button">Join Now</button> */}
       </div>
     </div>
   );
