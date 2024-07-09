48dea6e40873fed28fca8e30c110ca906c104ef3
graceJZ <jie.p.zhao@gmail.com>
2024-07-09 09:23:47 -0400

    footer improvement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
+            <div className="footer-image-square">
+                    <img src="https://raw.githubusercontent.com/graceJZ/hellograceimages/main/Sadhguru_Logo_Full_Black.png" alt="Sadhguru Logo" /> 
+                </div>
                 <div className="footer-image-wide">
                     <img src="https://raw.githubusercontent.com/graceJZ/hellograceimages/main/Hatha_Logo_Full_Black.png"
                     alt="Hatha Logo" />
                 </div>
-                <div className="footer-image-square">
-                    <img src="https://raw.githubusercontent.com/graceJZ/hellograceimages/main/Sadhguru_square_photo.jpg" alt="Sadhguru Logo" />
-                    <p>Classical Hatha Yoga<br />Structured by Sadhguru</p>
-                </div>
+             
             </div>
         </footer>
     );
